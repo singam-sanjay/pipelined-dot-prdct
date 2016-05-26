554cf2c24475feb852c76b648d243a9d18052079
Sanjay <sssv96@gmail.com>
2016-05-26 13:05:02 +0530

    1. Fixing the aim  : compute the results ( and optionally (-DDEBUG) write them back ) 2. Using only cudaMemcpy to move data ( Need to define function that would replace redundant code ) 3. Code with/without DEBUG compiles

--- load_func.cu
+++ load_func.cu
@@ -23,18 +23,13 @@ void __ld_data_frm_binfile( const char *file_name, size_t offset, const char *va
   }
 }
 
-void ld__frm_file_to_CPU( const char * dataset, const char * computed_result = NULL )
+void ld__frm_file_to_CPU( const char * dataset )
 {
   #define ld_cpu_MACRO(file,offset,var,size) { __ld_data_frm_binfile(file,offset,#var,var,size,__LINE__); }
   ld_cpu_MACRO(dataset, 0,cpu_vec,1*N);
   ld_cpu_MACRO(dataset,+N,cpu_mat,k*N);
-  #ifdef DEBUG
-  ld_cpu_MACRO(computed_result,0,cpu_res,k*N);
-  #endif
   #undef ld_cpu_MACRO
 }
-
-#include "cublas_v2.h"
 
 void __ld_CPU_to_GPU( TYPE* d_vec, TYPE* vec, size_t bytes, const char* var_name, const char* d_var_name )
 {
@@ -83,42 +78,42 @@ void rp__frm_rplca_to_wrkspc_on_GPU()
 #ifdef DEBUG
 void wb__to_CPU_frm_GPU()
 {
-  cublasStatus_t status;
-  status = cublasGetVector( size_vec,sizeof(TYPE),gpu_res,1,cpu_res,1 );
-  if( status!=CUBLAS_STATUS_SUCCESS )
+  cudaError_t status;
+  status = cudaMemcpy(cpu_res,gpu_res,k,cudaMemcpyDeviceToHost);
+  if( status==cudaSuccess )
   {
-    err_sstr << __func__ << "::gpu_res->cpu_res::";
-    switch( status )
-    {
-      case CUBLAS_STATUS_NOT_INITIALIZED: err_sstr << "The library was not initialized.\n";break;
-      case CUBLAS_STATUS_INVALID_VALUE: err_sstr << "The parameters incx , incy , elemSize<=0\n";break;
-      case CUBLAS_STATUS_MAPPING_ERROR: err_sstr << "There was an error accessing GPU memory\n";break;
-      default:			  err_sstr << "Got something else\n";break;
-    }
-    throw_str_excptn();
+    return;
   }
+  err_sstr << __func__ << "::" ;
+  switch( status )
+  {
+    case cudaErrorInvalidValue : err_sstr << "parameters passed to the API call is not within an acceptable range of values.\n";break;
+    case cudaErrorInvalidDevicePointer : err_sstr << "at least one device pointer passed to the API call is not a valid device pointer\n";break;
+    case cudaErrorInvalidMemcpyDirection 	 : err_sstr << "direction of the memcpy passed to the API call is not one of the types specified by cudaMemcpyKind\n";break;
+  }
+  throw_str_excptn();
 }
 
-void wb__to_file_frm_CPU()
+void wb__to_file_frm_CPU( const char *results )
 {
   /*
     Not writing any supporing function for this since only 1 variable has to written back
     */
   try
   {
-    ofstream f( file_name, std::ifstream::binary );
+    std::ofstream f( results, std::ifstream::binary );
     if( !f.is_open() )
     {
-      err_sstr << __func__ << "::Unable to open " << file_name << '\n';
+      err_sstr << __func__ << "::Unable to open " << results << '\n';
       throw_str_excptn();
     }
     f.exceptions( std::ifstream::failbit | std::ifstream::badbit | std::ifstream::eofbit );
-    f.write( ptr, sizeof(TYPE)*num );
+    f.write( (char*)cpu_res, sizeof(TYPE)*k );
     f.close();
   }
   catch( std::ios_base::failure &fail )
   {
-    err_sstr << __func__ << "::failure::" << file_name << "::" << fail.what() << '\n';
+    err_sstr << __func__ << "::failure::" << results << "::" << fail.what() << '\n';
     throw_str_excptn();
   }
 }
